ae39de071996a93ed4f78c7510a9d50d7bdeec34
Artzkaizen <arbeitchuka@gmail.com>
2024-05-02 12:41:48 +0200

    fix on all avatars

--- src/api/controllers/avatarControllers/getAvatarsController.js
+++ src/api/controllers/avatarControllers/getAvatarsController.js
@@ -5,9 +5,18 @@ const getAllAvatars = async(req, res) => {
         ensureFolderAndFilesExist(avatarFolderPath, avatarFilePath);
         const avatars = JSON.parse(await fs.promises.readFile(avatarFilePath));
 
-        avatars.length
-            ? res.status(200).json({message:'All your avatars!', avatars})
-            : res.status(200).json({message:'You dont have any avatars'})
+        // avatars.length
+        //     ? res.status(200).json({message:'All your avatars!', avatars})
+        //     : res.status(200).json({message:'You dont have any avatars'})
+
+        if (avatars && avatars.length) {
+            res.status(200).json({ message: 'All your avatars!', avatars });
+        } else if (avatars && !avatars.length) {
+            res.status(200).json({ message: 'You don\'t have any avatars' });
+        } else {
+            // Handle the case where avatars is undefined or not an array
+            res.status(200).json({ message: 'Avatars data is unavailable' });
+        }
 
     } catch (err) {
         console.error('Error reading avatars:', err);
@@ -21,14 +30,9 @@ const getOneAvatar = async(req, res) => {
         const avatars = JSON.parse( await fs.promises.readFile(avatarFilePath));
         const foundAvatar = avatars.find(avatar => avatar.id === req.params.id);
 
-        // foundAvatar
-        //     ? res.status(200).json({message: `Avatar found`, avatar: foundAvatar})
-        //     : res.status(404).json({message: `Avatar not found`})
-        if (foundAvatar) {
-            return res.status(200).json({ message: `Avatar found`, avatar: foundAvatar });
-        } else {
-            return res.status(404).json({ message: `Avatar not found` });
-        }
+        foundAvatar
+            ? res.status(200).json({message: `Avatar found`, avatar: foundAvatar})
+            : res.status(404).json({message: `Avatar not found`})
 
     } catch (err) {
         console.error('Error reading avatars:', err);
